361a1ad9b7667ef7d37cfc733380704e0ea0f6a8
aguyotdvlpt <aguyotfc@gmail.com>
2018-05-17 16:09:15 +0200

    creation de la page about

--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -1,5 +1,6 @@
 import { Component } from '@angular/core';
 import { NavController } from 'ionic-angular';
+import { AboutPage } from '../about/about';
 
 @Component({
   selector: 'page-home',
@@ -9,6 +10,14 @@ export class HomePage {
 
   constructor(public navCtrl: NavController) {
 
+  }
+
+  // Création de la fonction permettant d'aller sur la page about
+
+  goToAbout() {
+      //On change la page d'accueil par la page à propos au clic sur le bouton
+    this.navCtrl.setRoot(AboutPage);
+    
   }
 
 }
